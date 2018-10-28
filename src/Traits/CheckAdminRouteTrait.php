59173b569a1902c3eb13cab3ff02b70df32d32d3
Andrew Kolpikov <kolpikov.andrey@gmail.com>
2018-10-28 17:17:51 +0200

    1.0.9 CheckAdminRouteTrait improved

--- src/Traits/CheckAdminRouteTrait.php
+++ src/Traits/CheckAdminRouteTrait.php
@@ -3,6 +3,10 @@ namespace Kolpikov\Packages\Traits;
 
 use Illuminate\Support\Facades\Request;
 
+/**
+ * Trait CheckAdminRouteTrait
+ * @package Kolpikov\Packages\Traits
+ */
 trait CheckAdminRouteTrait
 {
     /**
@@ -10,8 +14,14 @@ trait CheckAdminRouteTrait
      *
      * @return bool
      */
-    public static function checkIfAdmin()
+    public static function checkIfAdmin(): bool
     {
-        return (Request::segment(1) == \Config::get('admin.prefix', 'admin')) ? true : false;
+        if ( \Config::get('admin.root_prefix', false) === true
+            || (Request::segment(1) === \Config::get('admin.prefix', 'admin'))
+        ) {
+            return true;
+        }
+
+        return false;
     }
 }
